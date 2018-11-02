47d4b2474d928a428dd7d1783372f340cefaacfc
jessicagmarshall <jessica.marshall14@gmail.com>
2018-11-02 13:42:43 -0400

    random row chosen each refresh

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,42 @@
 import React, { Component } from 'react'
 import ReactTable from 'react-table'
+import wavfile from './../audio/testfile1.wav'
+import Sound from 'react-sound'
 import 'react-table/react-table.css'
+
+const data = [{
+  input: 'socio-political motivation',
+  output: 'domestic terrorism'
+}, {
+  input: 'socio political motivation',
+  output: 'techno-utopia'
+}, {
+  input: 'lalala',
+  output: 'dummy data'
+}, {
+  input: 'dummy data',
+  output: 'lalala'
+}, {
+  input: 'socio political motivation',
+  output: 'techno-utopia'
+}, {
+  input: 'socio political motivation',
+  output: 'techno-utopia'
+}, {
+  input: 'socio political motivation',
+  output: 'techno-utopia'
+}]
+
+const columns = [{
+  Header: 'Input Tag',
+  accessor: 'input'
+}, {
+  Header: 'Output Tag',
+  accessor: 'output'
+}, {
+  Header: 'Audio ID',
+  accessor: 'audio'
+}]
 
 class Main extends Component {
   constructor () {
@@ -11,6 +47,17 @@ class Main extends Component {
     this.injectThProps = this.injectThProps.bind(this)
   }
 
+  componentWillMount () {
+    let startRow = this.getRandomInt(0, data.length)
+    this.setState({rowIndex: startRow, rowContent: {input: data[startRow].input, output: data[startRow].output}})
+  }
+
+  getRandomInt (min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
   injectThProps (state, rowInfo, column) {
     return {
       style: { display: 'none' }
@@ -18,42 +65,14 @@ class Main extends Component {
   }
 
   render () {
-    const data = [{
-      input: 'socio-political motivation',
-      output: 'domestic terrorism'
-    }, {
-      input: 'socio political motivation',
-      output: 'techno-utopia'
-    }, {
-      input: 'lalala',
-      output: 'dummy data'
-    }, {
-      input: 'dummy data',
-      output: 'lalala'
-    }, {
-      input: 'socio political motivation',
-      output: 'techno-utopia'
-    }, {
-      input: 'socio political motivation',
-      output: 'techno-utopia'
-    }, {
-      input: 'socio political motivation',
-      output: 'techno-utopia'
-    }]
-
-    const columns = [{
-      Header: 'Input Tag',
-      accessor: 'input'
-    }, {
-      Header: 'Output Tag',
-      accessor: 'output'
-    }, {
-      Header: 'Audio ID',
-      accessor: 'audio'
-    }]
-
     return (
       <div>
+        { this.state.rowIndex === 0
+          ? <Sound url={wavfile}
+            playStatus={Sound.status.PLAYING}
+          />
+          : null
+        }
         <ReactTable
           data={data}
           columns={columns}
