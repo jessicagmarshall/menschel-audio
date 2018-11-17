e71e02e77ca56fc2dcf15066a00a14726877edd2
jessicagmarshall <jessica.marshall14@gmail.com>
2018-11-17 14:05:08 -0500

    flashing works preliminarily on 1.8 second long clip

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,9 +25,9 @@ class Main extends Component {
       audio: '',
       paused: false,
       tagList: [0, 1, 2, 3, 4, 5], // eventually replace this with yonatan's actual tags
-      flashing: false, // is this the last n seconds of the clip?
       rowBlue: false,
-      flashingTime: 1 // how many seconds before the end should the bottom bar begin flashing
+      flashingTime: 1, // how many seconds before the end should the bottom bar begin flashing
+      numFlashes: 0
     }
     this.injectThProps = this.injectThProps.bind(this)
     this.generateSet = this.generateSet.bind(this)
@@ -54,9 +54,10 @@ class Main extends Component {
               rowContent: {input: data[nextRow].input, output: data[nextRow].output, id: data[nextRow].audio},
               audioUrl: 'http://localhost:3003/audio?id=' + nextRow
             })
+            // begin flashing this.state.flashingTime seconds before the end of the sound clip
+            setTimeout(this.flashBar, 1000 * (this.state.duration - this.state.flashingTime))
           })
       })
-    setTimeout(() => this.flashBar, 1000 * (this.state.duration - this.state.flashingTime)) // change
   }
 
   getNextIndex () {
@@ -93,7 +94,19 @@ class Main extends Component {
   }
 
   flashBar () {
-
+    this.setState({rowBlue: !this.state.rowBlue, numFlashes: this.state.numFlashes + 1})
+    let flashDuration
+    if (this.state.numFlashes < 15) {
+      this.state.numFlashes < 5
+        ? flashDuration = this.state.flashingTime / 2 / 5 // 1 second
+        : flashDuration = this.state.flashingTime / 2 / 10 // half second
+      setTimeout(
+        this.flashBar,
+        flashDuration
+      )
+    } else {
+      this.setState({numFlashes: 0, rowBlue: false})
+    }
   }
 
   render () {
@@ -117,7 +130,7 @@ class Main extends Component {
                   this.generateSet(rowInfo.index)
                 },
                 style: {
-                  background: rowInfo.index !== this.state.rowIndex ? 'white' : this.state.flashing && this.state.rowBlue ? 'blue' : 'yellow'
+                  background: rowInfo.index !== this.state.rowIndex ? 'white' : this.state.rowBlue ? 'blue' : 'yellow'
                 }
               }
             } else {
@@ -137,7 +150,7 @@ class Main extends Component {
           sortable={false}
           minRows={1}
           loadingText={null}
-          style={{position: 'fixed', width: '100%', backgroundColor: this.state.flashing && this.state.rowBlue ? 'blue' : 'yellow', bottom: 0, height: 60}}
+          style={{position: 'fixed', width: '100%', backgroundColor: this.state.rowBlue ? 'blue' : 'yellow', bottom: 0, height: 60}}
           getTdProps={(state, rowInfo, column, instance) => {
             return {
               onClick: (e, handleOriginal) => {
