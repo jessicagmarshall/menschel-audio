be72f057167da41b59face3a2effce8f1500ab1e
jessicagmarshall <jessica.marshall14@gmail.com>
2018-12-04 19:16:27 -0500

    version that works over LAN & wifi on raspberry pi

--- server/index.js
+++ server/index.js
@@ -8,15 +8,15 @@ let app = express()
 
 app.use(cors())
 
-app.listen(3003, function () {
+app.listen(0.0.0.0, function () {
   console.log('listening on port 3003')
 })
 
 app.get('/audio', function (req, res) {
   console.log(req.get('host'))
   let fileId = req.query.id
-  // let file = '../audio/testfile' + fileId + '.wav'
-  let file = '../audio/exampleClip.wav' // temporary until we have the clips
+  let file = '../audio/testfile' + fileId + '.wav'
+  // let file = '../audio/exampleClip.wav' // temporary until we have the clips
   fs.access(file, fs.constants.F_OK, (err) => {
     if (!err) {
       let rstream = fs.createReadStream(file)
@@ -30,8 +30,8 @@ app.get('/audio', function (req, res) {
 
 app.get('/audioDuration', function (req, res) {
   let fileId = req.query.id
-  // let file = '../audio/testfile' + fileId + '.wav'
-  let file = '../audio/exampleClip.wav' // temporary until we have the clips
+  let file = '../audio/testfile' + fileId + '.wav'
+  // let file = '../audio/exampleClip.wav' // temporary until we have the clips
   wavFileInfo.infoByFilename(file, function (err, info) {
     if (err) throw err
     res.setHeader('Content-Type', 'application/json')
